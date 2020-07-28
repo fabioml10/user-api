fc0b4eb60e492c84db1b01869add507af68af4cf
Fabio Muller <fabioml10@yahoo.com.br>
2020-07-28 01:47:01 +0000

    Tests created

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,50 @@
+require 'simplecov'
+SimpleCov.start
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    context 'when user is valid' do
+      it "all params are present" do
+        User.create( email: 'teste@create.com', password: '123456', password_confirmation: '123456' )
+        user = User.new( email: 'teste1@create.com', password: '123456', password_confirmation: '123456' ) 
+        expect(user).to be_valid
+      end
+    end
+
+    context 'when params are empty' do
+      it "email cant be blank" do
+        user = User.new( email: nil ) 
+        user.valid?   
+        expect(user.errors[:email]).to include("can't be blank")
+      end
+    
+      it "password cant be blank" do
+        user = User.new( password: nil ) 
+        user.valid?   
+        expect(user.errors[:password]).to include("can't be blank")
+      end
+    
+      it "password_confirmation cant be blank" do
+        user = User.new( password_confirmation: nil ) 
+        user.valid?   
+        expect(user.errors[:password_confirmation]).to include("can't be blank")
+      end
+    end
+
+    context 'when user is not valid' do
+    
+      it "password and password_conformation need to be equal" do
+        user = User.new( password: '123456', password_confirmation: '123456' ) 
+        expect(user.password).to eq(user.password_confirmation)
+      end
+    
+      it "email cant been taken" do 
+        user = User.create( email: 'contato@ironmaiden.com', password: '123456', password_confirmation: '123456' ) 
+        user = User.new( email: 'contato@ironmaiden.com', password: '123456', password_confirmation: '123456' ) 
+        user.valid? 
+        expect(user.errors[:email]).to include('has already been taken')
+      end
+    end
+  end
 end
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -1,5 +1,176 @@
+require 'simplecov'
+SimpleCov.start
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
+
+  let(:host) {"http://localhost:3001/api/v1/"}
+  let(:json) {JSON.parse(response.body)}
+
+  describe "GET users#index" do
+
+    it "responds with 200" do
+      get("#{host}/users")
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "returns an array object" do
+      get("#{host}/users")
+      expect(json).to be_an_instance_of(Array)
+    end
+
+    it "returns all users" do
+      user1 = User.create(email: "teste1@teste.com", password: "123456", password_confirmation: "123456")
+      user2 = User.create(email: "teste2@teste.com", password: "123456", password_confirmation: "123456")
+      get("#{host}/users")
+      expect(json.length).to be > 1
+    end
+
+  end
+
+  describe "GET users#show" do
+
+    it "responds with 200 and only one result and array object" do
+      user = User.create(email: 'teste@show.com', password: "123456", password_confirmation: "123456")
+      get("#{host}/users/#{user.id}")
+      expect(response).to have_http_status(:ok)
+      expect(json.length).to eq(1)
+      expect(json).to be_an_instance_of(Array)
+    end
+
+  end
+
+  describe "POST users#login" do
+
+  it 'responds with 201' do
+    user = User.create(email: 'teste@login.com', password: "123456", password_confirmation: "123456")
+    user_params = {
+      email: user.email,
+      password: user.password
+    }
+
+    post "#{host}/users/login", :params => user_params.to_json, :headers => { "Content-Type": "application/json" }
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'responds with 201' do
+    user = User.create(email: 'teste@login.com', password: "123456", password_confirmation: "123456")
+    user_params = {
+      email: user.email,
+      password: "wrong password"
+    }
+
+    post "#{host}/users/login", :params => user_params.to_json, :headers => { "Content-Type": "application/json" }
+    expect(response).to have_http_status(:unauthorized)
+  end
+
+  end
+
+  describe "POST users#create" do
+
+    it 'responds with 201' do
+      user_params = { user: {
+        email: 'johndoe@example.com',
+        password: '123456',
+        password_confirmation: '123456',
+      }}
+
+      post "#{host}/users", :params => user_params.to_json, :headers => { "Content-Type": "application/json" }
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'responds with 404' do
+      user_params = { user: {
+        email: 'johndoe@example.com',
+        password: '123456',
+        password_confirmation: nil,
+      }}
+
+      post "#{host}/users", :params => user_params.to_json, :headers => { "Content-Type": "application/json" }
+      expect(response).to have_http_status(:bad_request)
+    end
+
+  end
+
+  describe 'PATCH user#change' do
+    it 'should change password' do
+
+      user = User.create(email: 'teste@teste.com1', password: "123456", password_confirmation: "123456")
+      auth_token = JsonWebToken.encode({user_id: user.id})
+
+      new_activity_params = {
+        new_password: '654321',
+        password_confirmation: '654321',
+        token: auth_token
+      }
+
+      patch "#{host}/users/change", :params => new_activity_params.to_json, :headers => { "Content-Type": "application/json" }
+      expect(json).to include("status"=> 'Password changed')
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'invalid token' do
+
+      new_activity_params = {
+        new_password: '654321',
+        password_confirmation: '654321',
+        token: nil
+      }
+
+      patch "#{host}/users/change", :params => new_activity_params.to_json, :headers => { "Content-Type": "application/json" }
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it 'expired token' do
+
+      user = User.create(email: 'teste@change.com', password: "123456", password_confirmation: "123456")
+
+      token = {
+        exp: 1000000,
+        iss: 'issuer_name',
+        aud: 'clients',
+        user_id: user.id
+      }
+      
+      auth_token = JWT.encode(token, Rails.application.secrets.secret_key_base)
+
+      new_activity_params = {
+        new_password: '654321',
+        password_confirmation: '654321',
+        token: auth_token
+      }
+
+      patch "#{host}/users/change", :params => new_activity_params.to_json, :headers => { "Content-Type": "application/json" }
+      expect(response).to have_http_status(:unauthorized)
+    end
+    
+  end
+
+  describe 'DELETE user#destroy' do
+    it 'should delete user' do
+      user = User.create(email: 'teste@teste.com2', password: "123456", password_confirmation: "123456")
+      auth_token = JsonWebToken.encode({user_id: user.id})
+
+      new_activity_params = {
+        token: auth_token
+      }
+
+      delete "#{host}/users/#{user.id}", :params => new_activity_params.to_json, :headers => { "Content-Type": "application/json" }
+      expect(json).to include("status"=> 'User deleted.')
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should not delete user' do
+      user = User.new(id: 0, email: 'teste@teste.com2', password: "123456", password_confirmation: "123456")
+      auth_token = JsonWebToken.encode({user_id: user.id})
+
+      new_activity_params = {
+        token: auth_token
+      }
+
+      delete "#{host}/users/#{user.id}", :params => new_activity_params.to_json, :headers => { "Content-Type": "application/json" }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
 
 end
